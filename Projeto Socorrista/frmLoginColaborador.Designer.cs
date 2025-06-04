c6a055491ea3eaf4149927886966e02c042de870
cauagoncalves-p <cauagoncalves2190@gmail.com>
2025-06-04 11:55:50 -0300

    fazendo pagina de login

--- Projeto Socorrista/frmLoginColaborador.Designer.cs
+++ Projeto Socorrista/frmLoginColaborador.Designer.cs
@@ -30,10 +30,8 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.lblBemVindo = new System.Windows.Forms.Label();
             this.lblCriarConta = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
@@ -41,11 +39,14 @@
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.btnCriar = new System.Windows.Forms.Button();
-            this.lblsubTitulo = new System.Windows.Forms.Label();
-            this.lblsubTitulo1 = new System.Windows.Forms.Label();
             this.btnFazerLogin = new System.Windows.Forms.Button();
+            this.lblsubTitulo1 = new System.Windows.Forms.Label();
+            this.lblsubTitulo = new System.Windows.Forms.Label();
+            this.lblBemVindo = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
@@ -63,22 +64,12 @@
             this.panel1.Size = new System.Drawing.Size(393, 561);
             this.panel1.TabIndex = 0;
             // 
-            // lblBemVindo
-            // 
-            this.lblBemVindo.AutoSize = true;
-            this.lblBemVindo.Font = new System.Drawing.Font("Arial Narrow", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblBemVindo.Location = new System.Drawing.Point(99, 138);
-            this.lblBemVindo.Name = "lblBemVindo";
-            this.lblBemVindo.Size = new System.Drawing.Size(183, 43);
-            this.lblBemVindo.TabIndex = 0;
-            this.lblBemVindo.Text = "Bem vindo ";
-            // 
             // lblCriarConta
             // 
             this.lblCriarConta.AutoSize = true;
             this.lblCriarConta.Font = new System.Drawing.Font("Arial Narrow", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblCriarConta.ForeColor = System.Drawing.Color.White;
-            this.lblCriarConta.Location = new System.Drawing.Point(607, 87);
+            this.lblCriarConta.Location = new System.Drawing.Point(552, 22);
             this.lblCriarConta.Name = "lblCriarConta";
             this.lblCriarConta.Size = new System.Drawing.Size(176, 42);
             this.lblCriarConta.TabIndex = 1;
@@ -87,22 +78,12 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(559, 212);
+            this.textBox1.Location = new System.Drawing.Point(414, 149);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(354, 40);
+            this.textBox1.Size = new System.Drawing.Size(242, 29);
             this.textBox1.TabIndex = 2;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(501, 212);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(52, 40);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.pictureBox1.TabIndex = 3;
-            this.pictureBox1.TabStop = false;
             // 
             // pictureBox2
             // 
@@ -173,15 +154,17 @@
             this.btnCriar.Text = "Criar";
             this.btnCriar.UseVisualStyleBackColor = true;
             // 
-            // lblsubTitulo
+            // btnFazerLogin
             // 
-            this.lblsubTitulo.AutoSize = true;
-            this.lblsubTitulo.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblsubTitulo.Location = new System.Drawing.Point(98, 212);
-            this.lblsubTitulo.Name = "lblsubTitulo";
-            this.lblsubTitulo.Size = new System.Drawing.Size(184, 20);
-            this.lblsubTitulo.TabIndex = 1;
-            this.lblsubTitulo.Text = "Se você já é um colaborador";
+            this.btnFazerLogin.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnFazerLogin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnFazerLogin.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFazerLogin.Location = new System.Drawing.Point(102, 336);
+            this.btnFazerLogin.Name = "btnFazerLogin";
+            this.btnFazerLogin.Size = new System.Drawing.Size(169, 43);
+            this.btnFazerLogin.TabIndex = 11;
+            this.btnFazerLogin.Text = "Login";
+            this.btnFazerLogin.UseVisualStyleBackColor = false;
             // 
             // lblsubTitulo1
             // 
@@ -193,17 +176,54 @@
             this.lblsubTitulo1.TabIndex = 2;
             this.lblsubTitulo1.Text = "que contribuí com a gente, apenas realize o login a baixo";
             // 
-            // btnFazerLogin
+            // lblsubTitulo
             // 
-            this.btnFazerLogin.BackColor = System.Drawing.Color.CornflowerBlue;
-            this.btnFazerLogin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnFazerLogin.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnFazerLogin.Location = new System.Drawing.Point(102, 336);
-            this.btnFazerLogin.Name = "btnFazerLogin";
-            this.btnFazerLogin.Size = new System.Drawing.Size(169, 43);
-            this.btnFazerLogin.TabIndex = 11;
-            this.btnFazerLogin.Text = "Criar";
-            this.btnFazerLogin.UseVisualStyleBackColor = false;
+            this.lblsubTitulo.AutoSize = true;
+            this.lblsubTitulo.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblsubTitulo.Location = new System.Drawing.Point(98, 212);
+            this.lblsubTitulo.Name = "lblsubTitulo";
+            this.lblsubTitulo.Size = new System.Drawing.Size(184, 20);
+            this.lblsubTitulo.TabIndex = 1;
+            this.lblsubTitulo.Text = "Se você já é um colaborador";
+            // 
+            // lblBemVindo
+            // 
+            this.lblBemVindo.AutoSize = true;
+            this.lblBemVindo.Font = new System.Drawing.Font("Arial Narrow", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBemVindo.Location = new System.Drawing.Point(99, 138);
+            this.lblBemVindo.Name = "lblBemVindo";
+            this.lblBemVindo.Size = new System.Drawing.Size(183, 43);
+            this.lblBemVindo.TabIndex = 0;
+            this.lblBemVindo.Text = "Bem vindo ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(410, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 20);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(699, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 20);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Sobrenome";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(703, 149);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(242, 29);
+            this.textBox5.TabIndex = 14;
             // 
             // frmLogin
             // 
@@ -211,6 +231,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.CornflowerBlue;
             this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnCriar);
             this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.textBox4);
@@ -218,7 +241,6 @@
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.lblCriarConta);
             this.Controls.Add(this.panel1);
@@ -230,7 +252,6 @@
             this.Text = "Login";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
@@ -242,19 +263,21 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label lblBemVindo;
         private System.Windows.Forms.Label lblCriarConta;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Label lblsubTitulo1;
-        private System.Windows.Forms.Label lblsubTitulo;
         private System.Windows.Forms.PictureBox pictureBox4;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Button btnCriar;
         private System.Windows.Forms.Button btnFazerLogin;
+        private System.Windows.Forms.Label lblsubTitulo1;
+        private System.Windows.Forms.Label lblsubTitulo;
+        private System.Windows.Forms.Label lblBemVindo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox5;
     }
 }
---
+++ Projeto Socorrista/frmLoginVoluntario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_Socorrista
+{
+    public partial class frmLoginVoluntario : Form
+    {
+        public frmLoginVoluntario()
+        {
+            InitializeComponent();
+        }
+
+        private void frmLoginVoluntario_Load(object sender, EventArgs e)
+        {
+            fontes();
+        }
+
+
+        private void fontes() {
+            //lblTituloPrincipal.Font = new Font("Montserrat", 30, FontStyle.Bold);
+        }
+
+     
+    }
+}
